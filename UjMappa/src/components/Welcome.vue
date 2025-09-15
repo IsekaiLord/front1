033b6969cfd239828ee9d42d434cb473351d9e82
Sisekai <IsekaiLords@gmail.com>
2025-09-15 12:31:34 +0200

    Some basic changes

---
+++ UjMappa/src/components/Welcome.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ComponentName',
+  data() {
+    return {
+      
+    };
+  },
+  methods: {
+    
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  
+}
+</style>
